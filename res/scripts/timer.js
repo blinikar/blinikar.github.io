01d8eeb05a86f43e5f941bc6e46a60e8dab23bfa
Yegor Blinov <blinov.egor2011@yandex.ru>
2021-01-13 21:08:33 +0300

    Initial commit

---
+++ res/scripts/timer.js
@@ -0,0 +1,23 @@
+let USEDate = new Date(2021, 4, 25, 9, 0);
+let USESeconds = USEDate.getTime()
+
+function showTimer(){
+    let secondsBox = document.getElementById("seconds");
+    let minutesBox = document.getElementById("minutes");
+    let hoursBox = document.getElementById("hours");
+    let daysBox = document.getElementById("days");
+
+    let currentSeconds = new Date().getTime();
+
+    seconds = Math.floor((USESeconds - currentSeconds) / 1000);
+    minutes = Math.floor(seconds / 60);
+    hours = Math.floor(minutes / 60);
+    days = Math.floor(hours / 24);
+
+    secondsBox.innerHTML = seconds;
+    minutesBox.innerHTML = minutes;
+    hoursBox.innerHTML = hours;
+    daysBox.innerHTML = days;
+}
+
+let timerId = setInterval(showTimer, 500);
